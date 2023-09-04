919e8f4eb186002d3f05b92f2fe033e25b0000a0
Galina <galina.zalesskaya@intel.com>
2023-09-04 23:25:14 +0300

    Revert refactoring

--- openvino_xai/explain.py
+++ openvino_xai/explain.py
@@ -3,14 +3,17 @@
 
 from abc import ABC
 from abc import abstractmethod
-from typing import Optional, List
+from typing import Dict, Any, Optional, List
 
+import cv2
 import numpy as np
+from PIL import Image
+from tqdm import tqdm
 import openvino
 
 from openvino_xai.model import XAIModel, XAIClassificationModel
 from openvino_xai.parameters import ExplainParameters, PostProcessParameters
-from openvino_xai.saliency_map import ExplainResult, PostProcessor, TargetExplainGroup
+from openvino_xai.saliency_map import ClassificationResult, ExplainResult, PostProcessor, TargetExplainGroup
 from openvino_xai.utils import logger
 
 
@@ -88,9 +91,103 @@ class BlackBoxExplainer(Explainer):
 
 
 class RISEExplainer(BlackBoxExplainer):
-    def explain(self, data: np.ndarray) -> ExplainResult:
+    def __init__(self, model, num_masks=5000, num_cells=8, prob=0.5):
+        """RISE BlackBox Explainer
+
+        Args:
+            num_masks (int, optional): number of generated masks to aggregate
+            num_cells (int, optional): number of cells for low-dimensional RISE
+                random mask that later will be upscaled to the model input size
+            prob (float, optional): with prob p, a low-res cell is set to 1;
+                otherwise, it's 0. Default: ``0.5``.
+
+        """
+        super().__init__(model)
+        self.input_size = model.inputs["data"].shape[-2:]
+        self.num_masks = num_masks
+        self.num_cells = num_cells
+        self.prob = prob
+
+    def explain(
+        self,
+        data,
+        target_explain_group: Optional[TargetExplainGroup] = None,
+        explain_targets: Optional[List[int]] = None,
+        post_processing_parameters: Optional[Dict[str, Any]] = None,
+    ):
         """Explain the input."""
-        raise NotImplementedError
+        raw_saliency_map = self._generate_saliency_map(data)
+
+        resized_data = self._resize_input(data)
+        predicted_classes = self._model(resized_data)[0]
+        cls_result = ClassificationResult(predicted_classes, raw_saliency_map, np.ndarray(0), np.ndarray(0))
+        
+        target_explain_group = self._get_target_explain_group(target_explain_group)
+        explain_result = ExplainResult(cls_result, target_explain_group, explain_targets, self._labels)
+
+        processed_explain_result = self._get_processed_explain_result(
+            explain_result, data, post_processing_parameters
+        )
+
+        return processed_explain_result
+
+    def _generate_saliency_map(self, data):
+        """Generate RISE saliency map
+        Returns:
+            sal (np.ndarray): saliency map for each class
+
+        """
+        cell_size = np.ceil(np.array(self.input_size) / self.num_cells)
+        up_size = np.array((self.num_cells + 1) * cell_size, dtype=np.uint32)
+        rand_generator = np.random.default_rng(seed=42)
+
+        resized_data = self._resize_input(data)
+
+        sal_maps = []
+        for i in tqdm(range(0, self.num_masks), desc="Explaining"):
+            mask = self._generate_mask(cell_size, up_size, rand_generator)
+            # Add channel dimentions for masks
+            masked = np.expand_dims(mask, axis=2) * resized_data
+            scores = self._model(masked).raw_scores
+            sal = scores.reshape(-1, 1, 1) * mask
+            sal_maps.append(sal)
+        sal_maps = np.sum(sal_maps, axis=0)
+
+        sal_maps = self._normalize_saliency_maps(sal_maps)
+        sal_maps = np.expand_dims(sal_maps, axis=0)
+        return sal_maps
+
+    def _generate_mask(self, cell_size, up_size, rand_generator):
+        """Generate masks for RISE
+            cell_size (int): calculated size of one cell for low-dimensional RISE
+            up_size (int): increased cell size to crop
+            rand_generator (np.random.generator): generator with fixed seed to generate random masks  
+        Returns:
+            masks (np.array): self.num_masks float masks from 0 to 1 with size of input model
+
+        """
+        grid_size = (self.num_cells, self.num_cells)
+        grid = rand_generator.random(grid_size) < self.prob
+        grid = grid.astype(np.float32)
+
+        # Random shifts
+        x = np.random.randint(0, cell_size[0])
+        y = np.random.randint(0, cell_size[1])
+        # Linear upsampling and cropping
+        upsampled_mask = cv2.resize(grid, up_size, interpolation=cv2.INTER_LINEAR)
+        mask = upsampled_mask[x : x + self.input_size[0], y : y + self.input_size[1]]
+
+        return mask
+
+    def _resize_input(self, image):
+        image = cv2.resize(image, self.input_size, Image.BILINEAR)
+        return image
+
+    def _normalize_saliency_maps(self, saliency_map):
+        min_values = np.min(saliency_map)
+        max_values = np.max(saliency_map)
+        saliency_map = 255 * (saliency_map - min_values) / (max_values - min_values + 1e-12)
+        return saliency_map
 
 
 class DRISEExplainer(BlackBoxExplainer):
