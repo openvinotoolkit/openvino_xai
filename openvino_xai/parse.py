1768a48ed2ee45f8b00626e23572bff2427c9eb6
negvet <evgeny.tsykunov@intel.com>
2023-09-30 07:51:40 +0200

    Refactor parse.py

--- openvino_xai/parse.py
+++ openvino_xai/parse.py
@@ -19,17 +19,51 @@ class IRParser:
         )
         return logit_node
 
+    @staticmethod
+    def get_node_by_condition(ops, condition):
+        for op in ops:
+            if condition(op):
+                return op
+
+    @classmethod
+    def _is_conv_node_w_spacial_size(cls, op: openvino.runtime.Node) -> bool:
+        if op.get_type_name() != "Convolution":
+            return False
+        if not cls._has_spacial_size(op):
+            return False
+        return True
+
+    @classmethod
+    def _is_pooling_node_wo_spacial_size(cls, op: openvino.runtime.Node) -> bool:
+        if "Pool" not in op.get_friendly_name():
+            return False
+        if cls._has_spacial_size(op):
+            return False
+        return True
+
+    @staticmethod
+    def _has_spacial_size(
+            node: openvino.runtime.Node, output_id: int = 0, axis: Tuple[int] = (2, 3)
+    ) -> Optional[bool]:
+        node_output_shape = node.output(output_id).partial_shape
+
+        # NCHW
+        h, w, = node_output_shape[2].get_length(), node_output_shape[3].get_length()
+        # NHWC
+        h_, w_, = node_output_shape[1].get_length(), node_output_shape[2].get_length()
+
+        return (h != 1 and w != 1) or (h_ != 1 and w_ != 1)
+
+        # return h.get_length() > 1 and w.get_length() > 1
+
 
 class IRParserCls(IRParser):
     """ParserCls parse classification OV IR model."""
 
-    @staticmethod
-    def get_logit_node(model: openvino.runtime.Model, output_id=0) -> openvino.runtime.Node:
-        nodes = model.get_ops()
-        softmax_node = None
-        for op in nodes:
-            if "Softmax" == op.get_type_name():
-                softmax_node = op
+    @classmethod
+    def get_logit_node(cls, model: openvino.runtime.Model, output_id=0) -> openvino.runtime.Node:
+        reversed_ops = model.get_ordered_ops()[::-1]
+        softmax_node = cls.get_node_by_condition(reversed_ops, lambda x: x.get_type_name() == "Softmax")
         if softmax_node:
             logit_node = softmax_node.input(0).get_source_output().get_node()
             return logit_node
@@ -47,21 +81,27 @@ class IRParserCls(IRParser):
             cls, model: openvino.runtime.Model, output_backbone_node_name: Optional[str] = None
     ) -> openvino.runtime.Node:
         if output_backbone_node_name:
-            for op in model.get_ordered_ops():
-                if op.get_friendly_name() == output_backbone_node_name:
-                    return op
+            reversed_ops = model.get_ordered_ops()[::-1]
+            output_backbone_node = cls.get_node_by_condition(
+                reversed_ops, lambda x: x.get_friendly_name() == output_backbone_node_name
+            )
+            if output_backbone_node is not None:
+                return output_backbone_node
             raise ValueError(f"Cannot find {output_backbone_node_name} node.")
 
-        # Make an attempt to search for a first_head_node
+        # Make an attempt to search for last backbone node via a first head node
         first_head_node = cls.get_input_head_node(model)
         output_backbone_node = first_head_node.input(0).get_source_output().get_node()
-        spacial_shape = cls._has_spacial_shape(output_backbone_node)
-        if spacial_shape is not None:
-            if not spacial_shape:
-                # output_backbone_node suppose to have spacial dimensions
-                raise RuntimeError(f"Cannot find output backbone_node in auto mode, please provide target_layer.")
+        if cls._has_spacial_size(output_backbone_node):
+            return output_backbone_node
 
-        return output_backbone_node
+        # Make an attempt to search for a last conv node
+        reversed_ops = model.get_ordered_ops()[::-1]
+        last_conv_node = cls.get_node_by_condition(reversed_ops, cls._is_conv_node_w_spacial_size)
+        if last_conv_node is not None:
+            return last_conv_node
+
+        raise RuntimeError(f"Cannot find output backbone_node in auto mode, please provide target_layer.")
 
     @classmethod
     def get_input_head_node(
@@ -74,25 +114,10 @@ class IRParserCls(IRParser):
             assert len(target_input_nodes) == 1, "Support only single target input."
             return target_input_nodes[0]
 
-        for op in model.get_ordered_ops()[::-1]:
-            if cls._is_first_head_node(op):
-                return op
+        # Make an attempt to search for a last pooling node
+        reversed_ops = model.get_ordered_ops()[::-1]
+        last_pooling_node = cls.get_node_by_condition(reversed_ops, cls._is_pooling_node_wo_spacial_size)
+        if last_pooling_node is not None:
+            return last_pooling_node
 
         raise RuntimeError("Cannot find first head node in auto mode, please explicitly provide input parameters.")
-
-    @classmethod
-    def _is_first_head_node(cls, op: openvino.runtime.Node) -> bool:
-        checks = {
-            "last_pooling_layer": "Pool" in op.get_friendly_name() and not cls._has_spacial_shape(op),
-        }
-        return all(checks.values())
-
-    @staticmethod
-    def _has_spacial_shape(
-            node: openvino.runtime.Node, output_id: int = 0, axis: Tuple[int] = (2, 3)
-    ) -> Optional[bool]:
-        node_output_shape = node.output(output_id).partial_shape
-        h, w, = node_output_shape[axis[0]], node_output_shape[axis[1]]
-        dynamic = h.is_dynamic or w.is_dynamic
-        if not dynamic:
-            return h.get_length() > 1 and w.get_length() > 1
