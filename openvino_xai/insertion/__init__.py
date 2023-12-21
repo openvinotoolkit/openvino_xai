cd688f6c2a440d9301e2e7bb4c5bf2f00e705171
negvet <evgeny.tsykunov@intel.com>
2023-12-21 15:01:36 +0100

    Update inits

--- openvino_xai/insertion/__init__.py
+++ openvino_xai/insertion/__init__.py
@@ -1,14 +1,16 @@
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 """
-Interface to insertion Explainable AI (XAI) algorithms into OV IR.
+Interface for inserting eXplainable AI (XAI) branch into OV IR.
 """
-
+from openvino_xai.insertion.insertion_parameters import ClassificationInsertionParameters, \
+    DetectionInsertionParameters, InsertionParameters
 
 from openvino_xai.insertion.insert_xai_into_model import insert_xai
-from openvino_xai.insertion.insert_xai_into_model import insert_xai_into_mapi_wrapper
 
 __all__ = [
     "insert_xai",
-    "insert_xai_into_mapi_wrapper",
+    "InsertionParameters",
+    "ClassificationInsertionParameters",
+    "DetectionInsertionParameters",
 ]
