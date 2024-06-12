d68454bc81c4912cd02b7569274bbf13caf4cd6c
Songki Choi <songki.choi@intel.com>
2024-06-12 16:59:06 +0900

    Add fuzzing test

---
+++ tests/fuzz/test_fuzz.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import atheris
+import sys
+
+with atheris.instrument_imports():
+    from openvino_xai import Explainer, insert_xai
+
+def TestOneInput(input_bytes):
+    data_provider = atheris.FuzzedDataProvider(input_bytes)
+
+    # Test Explainer statefull class API
+    try:
+        explainer = Explainer(
+            model=data_provider.ConsumeBytes(8),
+            task=data_provider.ConsumeUInt(8),
+        )
+        explanation = explainer(data=data_provider.ConsumeBytes(8))
+    except ValueError as e:
+        pass
+
+    # Test insert_xai() stateless API
+    try:
+        insert_xai(
+            model=data_provider.ConsumeBytes(8),
+            task=data_provider.ConsumeUInt(8),
+        )
+    except ValueError as e:
+        pass
+
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
