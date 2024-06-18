c251a4602bbb743166e6bdca0f961e9c9b9c24f3
Songki Choi <songki.choi@intel.com>
2024-06-18 22:03:03 +0900

    Add fxt_cache_root

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import logging
+import os
 from pathlib import Path
 
 import pytest
@@ -18,12 +19,30 @@ def pytest_addoption(parser: pytest.Parser):
         default=".data",
         help="Data root directory.",
     )
+    parser.addoption(
+        "--cache-root",
+        action="store",
+        default="~/.cache",
+        help="Cache root directory.",
+    )
 
 
 @pytest.fixture(scope="session")
 def fxt_data_root(request: pytest.FixtureRequest) -> Path:
     """Data root directory path."""
-    data_root = Path(request.config.getoption("--data-root"))
+    data_root = Path(request.config.getoption("--data-root")).expanduser().absolute()
     msg = f"{data_root = }"
     log.info(msg)
     return data_root
+
+
+@pytest.fixture(scope="session", autouse=True)
+def fxt_cache_root(request: pytest.FixtureRequest) -> Path:
+    """Cache root directory path."""
+    cache_root = Path(request.config.getoption("--cache-root")).expanduser().absolute()
+    os.environ["XDG_CACHE_HOME"] = cache_root.as_posix()
+    os.environ["HF_HOME"] = cache_root.as_posix()
+    msg = f"{cache_root = }"
+    print(msg)
+    log.info(msg)
+    return cache_root
