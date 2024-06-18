348dcdb914724c674ae7ae0f344eb649d465de48
Songki Choi <songki.choi@intel.com>
2024-06-18 14:39:39 +0900

    Fix unit test

--- tests/unit/insertion/test_model_parser.py
+++ tests/unit/insertion/test_model_parser.py
@@ -2,14 +2,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import openvino.runtime as ov
+import pytest
 
 from openvino_xai.common.utils import retrieve_otx_model
 from openvino_xai.inserter.model_parser import IRParserCls
 from openvino_xai.inserter.parameters import ModelType
-from tests.intg.test_classification import DATA_DIR, DEFAULT_CLS_MODEL
+from tests.intg.test_classification import DEFAULT_CLS_MODEL
 
 
 class TestIRParser:
+    @pytest.fixture(autouse=True)
+    def setup(self, fxt_data_root):
+        self.data_dir = fxt_data_root
+
     def test_get_logit_node(self):
         model = self._get_model()
         logit_node = IRParserCls.get_logit_node(model)
@@ -26,6 +31,6 @@ class TestIRParser:
         assert "Convolution_" in first_conv_node.name
 
     def _get_model(self):
-        retrieve_otx_model(DATA_DIR, DEFAULT_CLS_MODEL)
-        model_path = DATA_DIR / "otx_models" / (DEFAULT_CLS_MODEL + ".xml")
+        retrieve_otx_model(self.data_dir, DEFAULT_CLS_MODEL)
+        model_path = self.data_dir / "otx_models" / (DEFAULT_CLS_MODEL + ".xml")
         return ov.Core().read_model(model_path)
