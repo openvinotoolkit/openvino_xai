42a4729416c03f67419a52d4248ca74118c75ce1
Songki Choi <songki.choi@intel.com>
2024-09-12 13:25:33 +0900

    Update doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 project = "OpenVINO™ XAI"
 copyright = "2024, Intel(R) Corporation"
 author = "Intel(R) Corporation"
-release = "1.0.0"
+release = "1.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
